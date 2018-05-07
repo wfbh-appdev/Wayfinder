44fd5b91ac245dca18d2c90548fb23d8536df38e
Christian Guerra <cguerra@phunware.com>
2018-05-07 11:35:04 -0700

    disabled level selector in map view.

--- Generic/Source/Custom/AFMMapViewController+WF.h
+++ Generic/Source/Custom/AFMMapViewController+WF.h
@@ -4,11 +4,11 @@
 //
 //  Created by Julio Estrada on 4/18/18.
 //  Copyright © 2018 Phunware. All rights reserved.
-//
 
-//#import <AppFrameworkMappingModule/AFMUIMapViewController.h>
-//
-//@interface AFMUIMapViewController (WF)
-//
-//@end
+
+#import <AppFrameworkMappingModule/AFMMapViewController.h>
+
+@interface AFMMapViewController (WF)
+
+@end
 
